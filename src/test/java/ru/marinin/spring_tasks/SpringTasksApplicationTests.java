07de36c5d33323691c7e20c62594ac23b515a02d
Easyfumi <oleg.marinin.49@yandex.ru>
2024-03-09 18:10:02 +0300

    Initial commit

---
+++ src/test/java/ru/marinin/spring_tasks/SpringTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.marinin.spring_tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
